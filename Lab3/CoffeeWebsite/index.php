e227f0297295add17e5d0cc11f0480fa1c9aa130
Garaba Dumitrita <dumitritag1@gmail.com>
2017-04-10 03:48:53 +0200

    create my project

---
+++ Lab3/CoffeeWebsite/index.php
@@ -0,0 +1,75 @@
+<?php
+$title = "Home";
+$content = '<img src="Images/coffee1.png" class="imgLeft" />
+<h3>Descriere</h3>
+<p>
+Cafeaua este o băutură de culoare neagră ce conține cafeină și care
+se obține din boabe de cafea prăjite și măcinate.
+Boabele de cafea sunt fructele arborilor de cafea din familia Rubiaceae,
+genul Coffea, cu două varietăți mai importante: Coffea arabica și Coffea 
+robusta (Coffea canephora). Coffea arabica furnizează sorturile cu 
+aromele cele mai fine, pe când Coffea robusta este mai amară și mai
+puțin aromată. Calitatea cafelei este influențată de locul de cultivare,
+depozitare și procesul de torefiere a boabelor de cafea.
+</p>
+
+<img src="Images/coffee2.png" class="imgRight" />
+<h3>Efecte pozitive</h3>
+<p>
+    Substanțele antioxidante din cafea (vitamina B, C, E, carotenul, lipidele),
+    protejează celulele renale de radicalii de oxigen cancerigen, în comparație
+    cu ceaiul verde la care nu s-a putut dovedi acest efect. După o perioadă de
+    zece ani de cercetări în Japonia, din 100 000 de persoane care au consumat
+    cafea, 214 s-au îmbolnăvit și au murit de cancer renal, în comparație cu 
+    neconsumatorii de cafea, dintre care au murit de cancer 514 persoane.
+    Rezultate asemănătoare s-au obținut în Ottawa la femeile cu tendință de 
+    cancer la sân. Un alt studiu făcut de National Health and Nutrition 
+    Examination Survey publicat în anul 2005, arată că la cei care consumă cel
+    puțin două căni de cafea zilnic se reduce incidența bolillor cronice de 
+    ficat, care sunt frecvente la alcoolici sau persoane supraponderale. Prin 
+    efectul cofeinei incidența bolii se reduce. Se presupune un efect pozitiv 
+    și în boala Parkinson sau Alzheimer.
+Cafeaua este o băutură care nu conține calorii și contribuie la completarea
+necesarului de substanțe minerale ca fier, potasiu, magneziu și mangan. De 
+asemenea, cafeaua este un furnizor important de vitamine cum ar fi niacina, o 
+vitamină din grupul B, care este răspunzătoare de aportul de energie, 
+riboflavină, acid pantotenic, vitamina E, vitamina C și vitamina B6.
+Stimulează pozitiv metabolismul prin efectului termogenic, o stimulare de a 
+arde grăsimile din corp, datorită cafeinei. Consumul de cafea, poate contribui
+la pierderea în greutate sau la menținerea ei la un anumit nivel.
+Cafeina are proprietatea de a activa combustia lipidelor din corp și de a 
+contribui prin aceasta la aportul energetic al organismului. Cafeina crește 
+nivelul adrenalinei, mărește tensiunea musculară și capacitatea de contracție a
+mușchilor. Studii recente arată că înaintea întrecerilor sportive, consumul de
+cafea are efecte pozitive asupra performanțelor și în plus, cafeina stimulează
+respirația, volumul pulmonar fiind exploatat mai eficient.
+Cafeaua facilitează digestia datorită efectului stimulent al cafeinei și 
+elementelor sale, stimulează secreția acidului gastric și al bilei.
+</p>
+
+<img src="Images/coffee3.png" class="imgLeft" />
+<h3>Efecte negative</h3>
+<p>
+    În cazul consumului abuziv de cafea sau alte băuturi care conțin cofeină 
+    pot să apară următoarele efecte negative:
+
+-tahicardie, (frecvență cardiacă crescută)
+
+-neliniște, tremor (tremurături), nervozitate, stări de panică
+
+-insomnie
+
+-convulsii
+
+-Anemie
+
+-o stare ușoară de dependență, în lipsa cafelei pot să apară dureri de cap 
+(migrenă)
+</p>';
+
+include 'Template.php';
+ ?> 
+
+
+
+
