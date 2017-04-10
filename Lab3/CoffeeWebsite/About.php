cdb69341f6be48ca81d951201d92bdf7569e126c
Garaba Dumitrita <dumitritag1@gmail.com>
2017-04-10 03:53:00 +0200

    about page

---
+++ Lab3/CoffeeWebsite/About.php
@@ -0,0 +1,22 @@
+<?php
+$title = "Descriere";
+$content = '<img src="Images/cafea.jpg" class="imgLeft" />
+<h3>Cafeaua este un moment de plăcere şi o aromă.</h3>
+<p>
+Sunt multe lucruri pe care trebuie să le iei în considerare atunci 
+când vrei să cumperi o cafea bună. Nu orice etichetă este o garanţie a 
+calităţii, însă cu cât un producător de cafea îţi poate spune mai multe
+despre cafeaua pe care urmează să o achiziţionezi, cu atât mai bine. 
+Absenţa informaţiilor este probabil mai gravă decât abundenţa lor.
+Sunt zeci de soiuri de cafea în întreaga lume şi toate au o aromă diferită.
+Există însă câteva generalităţi de care poţi ţine cont.
+"Altitudinea, compoziţia solului, ploile, tot ce pătrunde în pământul în 
+care creşte cafeaua contează". Acest website iti ofera top cele mai solicitate 
+de cafea si magazinele in care le puteti gasi.
+Enjoy!
+</p>';
+
+include 'Template.php';
+ ?> 
+
+
