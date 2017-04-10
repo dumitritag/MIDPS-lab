f592af5d9d40758a68c04ec8c846189f1fa7dad3
Garaba Dumitrita <dumitritag1@gmail.com>
2017-04-10 03:50:32 +0200

    template file

---
+++ Lab3/CoffeeWebsite/Template.php
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title><?php echo $title; ?></title>
+        <link rel="stylesheet" type="text/css" href="Styles/Stylesheet.css" />
+    </head>
+    <body>
+        <div id="wrapper">
+            <div id="banner">
+        </div>
+            <nav id="navigation">
+                <ul id="nav">
+                    <li><a href="index.php">Casa</a></li>
+                    <li><a href="Coffee.php">Cafea</a></li>
+                    <li><a href="Magazine.php">Magazine</a></li>
+                    <li><a href="About.php">Despre</a></li>
+                    <li><a href="Management.php">Management</a></li>
+                </ul>
+            </nav>
+            <div id="content_area">
+                <?php echo $content; ?>
+            </div>
+            <footer>
+                <li><a href="Calitate.php">Calitate</a></li>
+                <li><a href="feedback.php">Feedback</a></li> 
+            </footer>
+        </div>
+    </body>
+</html>
